681d4bdf76b4ebfa04b0bd3f71901aa641347f82
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-09-03 09:25:56 +0900

    [D4] Title: 물놀이를 가자, Time: 2,170 ms, Memory: 130,840 KB -BaekjoonHub

---
+++ SWEA/D4/10966. 물놀이를 가자/README.md
@@ -0,0 +1,15 @@
+# [D4] 물놀이를 가자 - 10966 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXWXMZta-PsDFAST) 
+
+### 성능 요약
+
+메모리: 130,840 KB, 시간: 2,170 ms, 코드길이: 1,531 Bytes
+
+### 제출 일자
+
+2024-09-03 09:25
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class Solution {
+	static int n, m;
+	static int[] dr = {0,1,0,-1}, dc = {1,0,-1,0};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= tc; t++) {
+			String[] str = br.readLine().split(" ");
+			n = Integer.parseInt(str[0]);
+			m = Integer.parseInt(str[1]);
+			int[][] mem = new int[n][m];
+			boolean[][] v = new boolean[n][m];
+			Queue<int[]> q = new ArrayDeque<int[]>();
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(mem[i], Integer.MAX_VALUE);
+				str = br.readLine().split("");
+				for (int j = 0; j < m; j++) {
+					if (str[j].equals("W")) {
+						mem[i][j] = 0;
+						v[i][j] = true;
+						int[] loc = {i, j};
+						q.add(loc);
+					}
+				}
+			}
+			while (!q.isEmpty()) {
+				int[] cur = q.poll();
+				for (int d = 0; d < 4; d++) {
+					int nr = cur[0] + dr[d], nc = cur[1] + dc[d];
+					if (nr >= 0 && nr < n && nc >= 0 && nc < m && !v[nr][nc] && mem[nr][nc] > mem[cur[0]][cur[1]] + 1) {
+						v[nr][nc] = true;
+						mem[nr][nc] = mem[cur[0]][cur[1]]+1;
+						int[] next = {nr, nc};
+						q.offer(next);
+					}
+				}
+			}
+			int ans = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					ans += mem[i][j];
+				}
+			}
+			System.out.println("#" + t + " " + ans);
+		}
+	}
+}
