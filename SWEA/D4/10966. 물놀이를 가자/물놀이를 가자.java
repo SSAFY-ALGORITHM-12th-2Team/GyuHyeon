eb80b9482ca45728d5c87c5d358b60d303504554
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-09-03 09:30:01 +0900

    [D4] Title: 물놀이를 가자, Time: 2,247 ms, Memory: 127,300 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -6,26 +6,21 @@ import java.util.Arrays;
 import java.util.Queue;
 
 public class Solution {
-	static int n, m;
 	static int[] dr = {0,1,0,-1}, dc = {1,0,-1,0};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= tc; t++) {
 			String[] str = br.readLine().split(" ");
-			n = Integer.parseInt(str[0]);
-			m = Integer.parseInt(str[1]);
-			int[][] mem = new int[n][m];
+			int n = Integer.parseInt(str[0]), m = Integer.parseInt(str[1]);
 			boolean[][] v = new boolean[n][m];
 			Queue<int[]> q = new ArrayDeque<int[]>();
 			for (int i = 0; i < n; i++) {
-				Arrays.fill(mem[i], Integer.MAX_VALUE);
 				str = br.readLine().split("");
 				for (int j = 0; j < m; j++) {
 					if (str[j].equals("W")) {
-						mem[i][j] = 0;
 						v[i][j] = true;
-						int[] loc = {i, j};
+						int[] loc = {i, j, 0};
 						q.add(loc);
 					}
 				}
@@ -35,11 +30,10 @@ public class Solution {
 				int[] cur = q.poll();
 				for (int d = 0; d < 4; d++) {
 					int nr = cur[0] + dr[d], nc = cur[1] + dc[d];
-					if (nr >= 0 && nr < n && nc >= 0 && nc < m && !v[nr][nc] && mem[nr][nc] > mem[cur[0]][cur[1]] + 1) {
+					if (nr >= 0 && nr < n && nc >= 0 && nc < m && !v[nr][nc]) {
 						v[nr][nc] = true;
-						mem[nr][nc] = mem[cur[0]][cur[1]]+1;
-						ans += mem[nr][nc];
-						int[] next = {nr, nc};
+						int[] next = {nr, nc, cur[2]+1};
+						ans += next[2];
 						q.offer(next);
 					}
 				}
