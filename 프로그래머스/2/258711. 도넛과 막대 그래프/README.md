fe96534ed06c4e8e0a24cc4acec0df08e8fcbe20
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-10-14 16:02:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 416.45 ms, Memory: 252 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
@@ -1,10 +1,10 @@
 # [level 2] 도넛과 막대 그래프 - 258711 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711) 
 
 ### 성능 요약
 
-메모리: 307 MB, 시간: 390.69 ms
+메모리: 252 MB, 시간: 416.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 14일 16:00:35
+2024년 10월 14일 16:02:41
 
 ### 문제 설명
 
--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -3,9 +3,7 @@ import java.util.*;
 class Solution {
     private int[] ans;
     public int[] solution(int[][] edges) {
-        ans = new int[4];
-        // 한 정점에서 dfs bfs 할 때 모든 정점 방문이 가능하면..?;
-        // 도넛: 사이클, 막대: 돌아갈 길 없음, 8: 한 정점에서 나오는 간선이 2개 이상인 노드 존재 
+        ans = new int[4]; // 도넛: 사이클, 막대: 돌아갈 길 없음, 8: 한 정점에서 나오는 간선이 2개 이상인 노드 존재 
         ArrayList<Integer>[] g = new ArrayList[1000001];
         for (int i = 1; i <= 1000000; i++) g[i] = new ArrayList<>();
         int[] inner = new int[1000001];
