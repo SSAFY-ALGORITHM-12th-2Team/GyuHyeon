f2f4e1c91ece2f0ff506172a39cd8ae93fdf5638
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-10-29 09:26:00 +0900

    Time: 178 ms (8.7%), Space: 74.1 MB (73.46%) - LeetHub

--- Leetcode/Medium/2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ Leetcode/Medium/2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j < m; j++) {
-                if (i-1 >= 0 && grid[i][j] > grid[i-1][j-1]) dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
-                if (grid[i][j] > grid[i][j-1]) dp[i][j] = max(dp[i][j], dp[i][j-1]+1);
-                if (i+1 < n && grid[i][j] > grid[i+1][j-1]) dp[i][j] = max(dp[i][j], dp[i+1][j-1]+1);
-                if (dp[i][j] == 0) break;
-                ans = max(ans, dp[i][j] - 1);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = (grid[j][i+1] > grid[j][i] ? dp[i+1][j] + 1 : 0);
+                if (j > 0 && grid[j][i] < grid[j-1][i+1]) dp[i][j] = max(dp[i][j], dp[i+1][j-1]+1);
+                if (j + 1 < n && grid[j][i] < grid[j+1][i+1]) dp[i][j] = max(dp[i][j], dp[i+1][j+1]+1);
             }
         }
-        return ans;
+        return *max_element(dp[0].begin(), dp[0].end());
     }
 };
