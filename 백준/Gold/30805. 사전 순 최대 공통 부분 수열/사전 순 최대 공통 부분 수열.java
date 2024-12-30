d8c413b319c01d718849db324a7e2be6dfa1083c
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-12-30 23:48:14 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 116 ms, Memory: 14740 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
@@ -10,50 +10,25 @@ public class Main {
         int[] b = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int maxA = Arrays.stream(a).max().orElse(0), maxB = Arrays.stream(b).max().orElse(0);
-        int maxval = Math.max(maxA, maxB);
+        int maxval = Math.max(maxA, maxB); // 전체 배열 내 최댓값 찾기
 
         List<Integer> ans = new ArrayList<>();
-        for (int val = maxval; val > 0; val--) {
+        for (int val = maxval; val > 0; val--) { // 최댓값부터 체크
             while (true) {
                 int ia = findIndex(a, val), ib = findIndex(b, val);
                 if (ia == -1 || ib == -1) break;
                 ans.add(val);
-                a = Arrays.copyOfRange(a, ia + 1, a.length);
-                b = Arrays.copyOfRange(b, ib + 1, b.length);
+                a = Arrays.copyOfRange(a, ia + 1, a.length); // 최댓값 있는 자리부터 뒤쪽까지 복사
+                b = Arrays.copyOfRange(b, ib + 1, b.length); // 최댓값보다 앞쪽의 값은 알 필요 없음
             }
         }
-
         System.out.println(ans.size());
         ans.forEach(x -> System.out.print(x + " "));
     }
-
     private static int findIndex(int[] arr, int val) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == val) return i;
         }
         return -1;
     }
-    
-//     public static void main(String[] args) throws Exception {
-// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// 		int n = Integer.parseInt(br.readLine());
-// 		String[] arr = br.readLine().split(" ");
-// 		int m = Integer.parseInt(br.readLine());
-// 		String[] arr2 = br.readLine().split(" ");
-// 		ArrayList<Integer>[][] dp = new ArrayList<Integer>[n+1][m+1];
-// 		for (int i = 1; i <= n; i++) dp[i][0] = arr[i-1];
-// 		for (int j = 1; j <= m; j++) dp[0][j] = arr2[j-1];
-// 		for (int i = 1; i <= n; i++) { 
-// 		    for (int j = 1; j <= m; j++) {
-// 		        dp[i][j] = new ArrayList<Integer>();
-// 		        if (arr[i-1].equals(arr2[j-1])) dp[i][j] = (dp[i-1][j].length() > dp[i][j-1].length() ? Arrays.copyOf(dp[i-1][j], dp[i-1][j].length()) : dp[i][j-1]) + arr[i-1];
-// 		        else dp[i][j] = (dp[i-1][j].length() > dp[i][j-1].length() ? dp[i-1][j] : dp[i][j-1]);
-// 		    }
-// 		}
-// 		String lcs = dp[n][m];
-// 		int idx = 0;
-// 		for (int i = 1; i < lcs.length(); i++) if (lcs.charAt(i) > lcs.charAt(idx)) idx = i;
-// 		System.out.println(lcs.length() - idx);
-// 		for (int i = idx; i < lcs.length(); i++) System.out.print(lcs.charAt(i) + " ");
-// 	}
 }
