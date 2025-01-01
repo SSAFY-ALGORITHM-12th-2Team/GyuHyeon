376b581a86489492bff96b5043adb2c9a32fc670
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2025-01-01 15:12:29 +0900

    Time: 0 ms (100%), Space: 8.8 MB (38.26%) - LeetHub

---
+++ Leetcode/Easy/1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int ones = count(s.begin(), s.end(), '1'), zeros = 0, ans = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') ones--;
+            else zeros++;
+            ans = max(ans, zeros + ones);
+        }
+        return ans;
+    }
+};
